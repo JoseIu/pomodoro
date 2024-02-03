887b27e22b1cf16a4f1684bad53fde106dd4cb58
Joselu <joseluissgz97@gmail.com>
2024-02-03 11:49:40 +0100

    openForm and closeForm

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ClockComponent } from '../clock/clock.component';
 
 @Component({
   selector: 'app-timer',
   standalone: true,
-  imports: [ClockComponent],
+  imports: [ClockComponent, CommonModule],
   templateUrl: './timer.component.html',
   styleUrl: './timer.component.scss',
 })
-export class TimerComponent {}
+export class TimerComponent {
+  public isOpen: boolean = false;
+  public openForm() {
+    this.isOpen = !this.isOpen;
+    console.log(this.isOpen);
+  }
+
+  public closeForm() {
+    this.isOpen = !this.isOpen;
+  }
+}
