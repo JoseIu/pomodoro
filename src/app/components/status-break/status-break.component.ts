b5d799b044f0cfe4429ac08ff1e9554f4600a1b5
Joselu <joseluissgz97@gmail.com>
2024-02-03 17:11:06 +0100

    status-break component

---
+++ src/app/components/status-break/status-break.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'status-break',
+  standalone: true,
+  imports: [],
+  templateUrl: './status-break.component.html',
+  styleUrl: './status-break.component.scss',
+})
+export class StatusBreakComponent {}
