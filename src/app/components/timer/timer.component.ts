ed83033e728a4d48bfa19d25dc6489c3681f51f2
Joselu <joseluissgz97@gmail.com>
2024-02-03 12:48:03 +0100

    form and submit values to clock component if form is valid

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,22 +1,68 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ClockComponent } from '../clock/clock.component';
 
 @Component({
   selector: 'app-timer',
   standalone: true,
-  imports: [ClockComponent, CommonModule],
+  imports: [ClockComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './timer.component.html',
   styleUrl: './timer.component.scss',
 })
 export class TimerComponent {
+  private fb = inject(FormBuilder);
   public isOpen: boolean = false;
+  public workTime: number = 0;
+  public breakTime: number = 0;
+
+  public myForm: FormGroup = this.fb.group({
+    workTime: [0, [Validators.required, Validators.min(25)]],
+    breakTime: [0, [Validators.required, Validators.min(5)]],
+  });
+
   public openForm() {
     this.isOpen = !this.isOpen;
-    console.log(this.isOpen);
   }
 
   public closeForm() {
     this.isOpen = !this.isOpen;
+  }
+
+  isValiedField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFielError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'The camp is required';
+        case 'min':
+          return `Minimun ${errors['min'].min} minutes`;
+      }
+    }
+    return null;
+  }
+  onSubmit() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    this.workTime = this.myForm.controls['workTime'].value;
+    this.breakTime = this.myForm.controls['breakTime'].value;
+    this.myForm.reset();
+    this.isOpen = false;
   }
 }
