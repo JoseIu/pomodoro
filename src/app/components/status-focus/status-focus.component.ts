190492cf6eed2c75480df9e449aa3ba33e37b7ec
Joselu <joseluissgz97@gmail.com>
2024-02-03 17:11:19 +0100

    status-focus component

---
+++ src/app/components/status-focus/status-focus.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'status-focus',
+  standalone: true,
+  imports: [],
+  templateUrl: './status-focus.component.html',
+  styleUrl: './status-focus.component.scss',
+})
+export class StatusFocusComponent {}
