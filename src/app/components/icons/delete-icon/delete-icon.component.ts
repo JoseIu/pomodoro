d063f70010bb9dbdb0929b3ee7cd14678e0774ca
Joselu <joseluissgz97@gmail.com>
2024-02-04 10:35:18 +0100

    Delete Icon

---
+++ src/app/components/icons/delete-icon/delete-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'delete-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './delete-icon.component.html',
+  styleUrl: './delete-icon.component.scss',
+})
+export class DeleteIconComponent {}
