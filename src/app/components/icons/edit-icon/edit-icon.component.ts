ecb10ec4e626967ba2ec120b8e988d8d2f5360cd
Joselu <joseluissgz97@gmail.com>
2024-02-04 10:35:30 +0100

    Edit Icon

---
+++ src/app/components/icons/edit-icon/edit-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'edit-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-icon.component.html',
+  styleUrl: './edit-icon.component.scss',
+})
+export class EditIconComponent {}
