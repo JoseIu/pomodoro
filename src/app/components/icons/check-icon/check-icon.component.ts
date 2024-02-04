e7947e49cd3660cd7a696bad060214b711547edb
Joselu <joseluissgz97@gmail.com>
2024-02-04 10:34:57 +0100

    Check Icon

---
+++ src/app/components/icons/check-icon/check-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'check-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './check-icon.component.html',
+  styleUrl: './check-icon.component.scss',
+})
+export class CheckIconComponent {}
