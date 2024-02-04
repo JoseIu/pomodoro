71433d254d5e4000673ab1e76e3f1953102f9808
Joselu <joseluissgz97@gmail.com>
2024-02-04 10:35:06 +0100

    Circle Icon

---
+++ src/app/components/icons/circle-icon/circle-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'circle-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './circle-icon.component.html',
+  styleUrl: './circle-icon.component.scss',
+})
+export class CircleIconComponent {}
